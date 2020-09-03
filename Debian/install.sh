8680eb2e216a02bdaf1da0dcaea7a61314a30985
Alvaro Karsz <alvaro.karsz@solid-run.com>
2020-09-03 16:44:09 +0300

    First commit - Debian

---
+++ Debian/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Downloading & Flashing Image"
+
+curl -L -k https://images.solid-build.xyz/IMX8/Debian/sr-imx8-debian-bullseye-20191201-cli-imx8mm-sdhc.img.xz --progress | unxz | dd of=/dev/mmcblk0 bs=1M conv=fsync
+
+sync
