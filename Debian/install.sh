87bfdcfb5bb51d12a76ad8aa8d3fb9d24df293e3
Alvaro Karsz <alvaro.karsz@solid-run.com>
2020-09-21 13:32:14 +0300

    let the operator choose the dd destination

--- Debian/install.sh
+++ Debian/install.sh
@@ -2,6 +2,4 @@
 
 echo "Downloading & Flashing Image"
 
-curl -L -k https://images.solid-build.xyz/IMX8/Debian/sr-imx8-debian-bullseye-20191201-cli-imx8mm-sdhc.img.xz --progress | unxz | dd of=/dev/$DESTINATION bs=1M conv=fsync
-
-sync
+curl -L -k https://images.solid-build.xyz/IMX8/Debian/sr-imx8-debian-bullseye-20200725-preview-cli-imx8mm-sdhc.img.xz --progress | unxz | dd of=$DESTINATION bs=1M conv=fsync && sync
